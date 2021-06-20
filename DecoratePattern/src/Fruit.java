fbc0effa2c7e53d6dde6644afcbb35a8ee072f32
nvtongyny <66721047+nvtongyny@users.noreply.github.com>
2021-06-20 17:07:54 +0800

    Add files via upload

---
+++ DecoratePattern/src/Fruit.java
@@ -0,0 +1,10 @@
+public class Fruit extends Decorating {
+    public Fruit(IBirthdayCake birthdayCake) {
+        super(birthdayCake);
+    }
+
+    public void PutFruit() {
+        System.out.println("Add Fruit.....");
+    }
+
+}
---
+++ DecoratePattern/src/IBirthdayCake.java
@@ -0,0 +1,5 @@
+public interface IBirthdayCake {
+
+    public void Show();
+
+}
