69af2efed4ad5e9dc64eff13c7548c38a3dbf6b3
nvtongyny <66721047+nvtongyny@users.noreply.github.com>
2021-06-20 17:13:50 +0800

    Delete FlyweighttPattern/src directory

--- FlyweighttPattern/src/APiece.java
+++
@@ -1,8 +0,0 @@
-public abstract class APiece {
-    protected String inKind;
-    public APiece(String inKind){
-        this.inKind = inKind;
-    }
-    //下棋时落子的位置
-    public abstract void Play(int x,int y);
-}
