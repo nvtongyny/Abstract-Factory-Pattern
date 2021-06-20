f7b1d46cd1177964f173dededde422b746cd6af9
nvtongyny <66721047+nvtongyny@users.noreply.github.com>
2021-06-20 17:22:44 +0800

    Add files via upload

---
+++ FlyweighttPattern/src/APiece.java
@@ -0,0 +1,8 @@
+public abstract class APiece {
+    protected String inKind;
+    public APiece(String inKind){
+        this.inKind = inKind;
+    }
+    //下棋时落子的位置
+    public abstract void Play(int x,int y);
+}
